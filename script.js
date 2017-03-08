3317dc6ab05f9e7a8cb6515228be263cef4d9448
Mikhail Khosenko <simpson452009@gmail.com>
2017-03-08 19:02:47 +0300

    Changed

---
+++ script.js
@@ -0,0 +1,12 @@
+var firstName = 'Mikhail';
+var lastName = 'Khosenko';
+var age = 23;
+
+alert('First Name = '+ firstName);
+alert('Last Name = '+ lastName);
+alert('Age = '+ age);
+
+function alertFunction(){
+	alert('Hello');
+}
+
